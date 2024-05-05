88dbd317b3a30c8f45aa0feb0f6daa3720533181
zubair khakwani <zbrkhakwani@gmail.com>
2024-05-05 12:17:05 +0500

    Add project files.

---
+++ BlazorCacheIssue/BlazorCacheIssue/Components/Layout/MainLayoutTop.razor.js
@@ -0,0 +1,11 @@
+﻿export function onLoad() {
+    console.log("SSR top layout on load");
+}
+
+export function onUpdate() {
+    console.log("SSR top layout on update");
+}
+
+export function onDispose() {
+    console.log("SSR top layout on dispose");
+}
---
+++ BlazorCacheIssue/BlazorCacheIssue/Components/Pages/Home.razor.js
@@ -0,0 +1,36 @@
+﻿// Called when the script first gets loaded on the page.
+export function onLoad() {
+    console.log("home load");
+}
+
+export function onUpdate() {
+    console.log("home");
+    AnimatedScrolling();
+
+    window.addEventListener("scroll", function () {
+        console.log("home3");
+
+        AnimatedScrolling();
+    });
+}
+
+function AnimatedScrolling() {
+    console.log("home scrolling");
+
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+
+        var revealTop = reveals[i].getBoundingClientRect().top;
+
+        var revealspoint = 150;
+
+        if (revealTop < windowHeight - revealspoint) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
